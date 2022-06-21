35b2673e13f5616a6afac80f6915acf023f04600
Anabel Batista <55217064+anabel-b@users.noreply.github.com>
2022-06-21 14:55:37 -0400

    Add files via upload

--- PeriodBleeding.java
+++ PeriodBleeding.java
@@ -3,23 +3,12 @@ package com.example.periodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class PeriodBleeding extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-    }
-
-    public void PeriodStarts(View view) {
-    }
-
-    public void noPeriod(View view) {
-
+        setContentView(R.layout.activity_period_bleeding);
     }
 }
