cfebd520016c2f8f0b62800ecbbfd499624a18fc
Anabel Batista <55217064+anabel-b@users.noreply.github.com>
2022-06-23 15:02:35 -0400

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,11 +2,14 @@ package com.example.periodtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button periodToday;
+    private Button noPeriodToday;
 
 
     @Override
@@ -14,12 +17,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    periodToday = findViewById(R.id.period_today_button);
+    noPeriodToday = findViewById(R.id.no_period_today_button);
+
+    periodToday.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //@haylin change this so that it goes together with what you have!
+            startActivity(new Intent(MainActivity.this,PeriodBleeding.class));
+        }
+    });
+
+    noPeriodToday.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //@haylin change this so that it goes together with what you have!
+            startActivity(new Intent(MainActivity.this,SurveyScreen.class));
+        }
+    });
+
     }
 
-    public void PeriodStarts(View view) {
-    }
 
-    public void noPeriod(View view) {
 
-    }
+
 }
--- SurveyScreen.java
+++ SurveyScreen.java
@@ -3,17 +3,12 @@ package com.example.periodtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class PeriodBleeding extends AppCompatActivity {
+public class SurveyScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_period_bleeding);
-    }
-
-    public void Spotting(View view) {
-
+        setContentView(R.layout.activity_survey_screen);
     }
 }
